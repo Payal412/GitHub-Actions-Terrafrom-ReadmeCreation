669956ccb5c46984d8afc9127c94b1f7c5a47126
Payal412 <bhute.payal@gmail.com>
2025-04-18 14:45:58 +0530

    Update variables.tf
    Give default value

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "file_content" {
   description = "Content to write into the file"
   type        = string
+  default     = "Hello, Payal! This is from Terraform with everything in main.tf"
 }
 
 variable "file_name" {
   description = "Name of the file to be created"
   type        = string
+  default     = "hello.txt"
 }
